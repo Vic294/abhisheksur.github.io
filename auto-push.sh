5b31ba4bcf9b5629a0b4a00f5425268c250b8fd4
Vic294 <abhisheksur429@gmail.com>
2025-04-14 14:40:40 +0000

    Auto update: Mon 14 Apr 2025 02:40:40 PM UTC

--- auto-push.sh
+++ auto-push.sh
@@ -7,8 +7,11 @@ git_push() {
   # Set the repository URL with the token for authentication
   REPO_URL="https://${GITHUB_TOKEN}@github.com/Vic294/abhisheksur.github.io.git"
   
-  # Add all changes
+  # Add all changes except workflow files
   git add .
+  
+  # Remove .github/workflows from staging
+  git reset -- .github/workflows
   
   # Commit with timestamp
   git commit -m "Auto update: $(date)"
